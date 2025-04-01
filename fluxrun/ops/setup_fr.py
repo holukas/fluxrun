fe965fa70ed19e7351ddbaa13550ac114ab664b5
Lukas Hörtnagl <31772692+holukas@users.noreply.github.com>
2025-04-01 14:53:35 +0200

    Use settings from YAML

--- fluxrun/ops/setup_fr.py
+++ fluxrun/ops/setup_fr.py
@@ -2,24 +2,30 @@ import os
 import time
 from pathlib import Path
 
+import yaml
+
 
 def read_settings_file_to_dict(dir_settings, file, reset_paths):
     """Read start values from settings file as strings into dict, with same variable names as in file"""
 
-    settings_dict = {}
     settings_file_fullpath = os.path.join(dir_settings, file)
+    with open(settings_file_fullpath, 'r', encoding='utf-8') as f:
+        settings_dict = yaml.safe_load(f)
 
-    with open(settings_file_fullpath) as input_file:
-        for line in input_file:  # cycle through all lines in settings file
-            if ('=' in line) and (not line.startswith('#')):  # identify lines that contain setting
-                line_id, line_setting = line.strip().split('=')
-
-                # reset all file paths, folder paths and instr info, will be filled during run
-                if reset_paths:
-                    if line_id.startswith('f_') or line_id.startswith('dir_') or line_id.startswith('instr_'):
-                        line_setting = ''
-
-                settings_dict[line_id] = line_setting  # store setting from file in dict
+    # settings_dict = {}
+    # settings_file_fullpath = os.path.join(dir_settings, file)
+    #
+    # with open(settings_file_fullpath) as input_file:
+    #     for line in input_file:  # cycle through all lines in settings file
+    #         if ('=' in line) and (not line.startswith('#')):  # identify lines that contain setting
+    #             line_id, line_setting = line.strip().split('=')
+    #
+    #             # reset all file paths, folder paths and instr info, will be filled during run
+    #             if reset_paths:
+    #                 if line_id.startswith('f_') or line_id.startswith('dir_') or line_id.startswith('instr_'):
+    #                     line_setting = ''
+    #
+    #             settings_dict[line_id] = line_setting  # store setting from file in dict
 
     return settings_dict
 
