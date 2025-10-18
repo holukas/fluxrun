ecf93fa5d2b1d96d71ae2bb610cfacbbe7931a91
Lukas Hörtnagl <lukas.hoertnagl@usys.ethz.ch>
2025-10-18 18:02:02 +0200

    Remove outdated comments and redundant code in `main.py`.

--- fluxrun/main.py
+++ fluxrun/main.py
@@ -8,34 +8,8 @@ from . import fluxrun_cli
 from . import fluxrun_gui
 from .cli import cli
 
-# todo testing
 print(f"{__file__}")
 
-
-# todo wrong imports
-# todo now solved? with adjusted batch script that sets script dir it seems to work correctly
-# todo batch script:
-# :: Activate the conda environment required for the script
-# call C:\Users\nopan\miniconda3\Scripts\activate.bat C:\Users\nopan\miniconda3\envs\fluxrun
-#
-# :: Change directory to the parent of 'fluxrun'
-# set SCRIPT_DIR=%~dp0
-# cd "%SCRIPT_DIR%"
-#
-# :: Execute the script as a module
-# :: This tells Python to look for the 'fluxrun' package in the current directory
-# call python -m fluxrun.main -g
-#
-# :: Optional: Deactivate the conda environment if you don't need it active afterward
-# :: call conda deactivate
-
-# Imports for some reason from script origin folder, should be from testfolder "del":
-#   F:\TMP\del\fluxrun\cli\cli.py
-#   F:\TMP\del\fluxrun\gui\mainwindow.py
-#   todo F:\Sync\luhk_work\20 - CODING\23 - FLUX\fluxrun\fluxrun\fluxrun_gui.py
-#   todo F:\Sync\luhk_work\20 - CODING\23 - FLUX\fluxrun\fluxrun\fluxrun_cli.py
-#   Working directory: F:\TMP\del\fluxrun
-#   F:\TMP\del\\fluxrun\main.py
 
 def main(args):
     abspath = Path(os.path.abspath(__file__)).parent  # directory of bico.py
@@ -43,7 +17,6 @@ def main(args):
     wd = os.getcwd()
     print(f"Working directory: {wd}")
 
-    # todo testing
     print(f"{__file__}")
 
     # Run FLUXRUN w/o GUI
